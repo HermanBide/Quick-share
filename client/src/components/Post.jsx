a073f1b829406b5e8c4d75c5f3c07d27a9c440fc
hermanb <bideherman@yahoo.com>
2021-11-08 06:05:35 -0800

    updated post div containers and post css

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -37,21 +37,21 @@ const Post = (props) => {
       {posts.map((post) => (
         <div className="post_card">
           <div className="titles">
-            <div>Movie Title</div>
-            <div>Release date</div>
-            <div>Director</div>
-            <div>Review</div>
-            <div>Rating</div>
-            <div>Genre</div>
+            <div className="Review_div">Movie Title</div>
+            <div className="Review_div">Release date</div>
+            <div className="Review_div">Director</div>
+            <div className="Review_div">Review</div>
+            <div className="Review_div">Rating</div>
+            <div className="Review_div">Genre</div>
           </div>
 
           <div className="post_info">
-            <div><h3>{post.title}</h3></div>
-            <div><h4>{post.release_date}</h4></div>
-            <div><h4>{post.director}</h4></div>
-            <div><h4>{post.review}</h4></div>
-            <div><h4>{post.rating}</h4></div>
-            <div> <h4>{post.genre}</h4></div>
+            <div className="Review_div"><h3>{post.title}</h3></div>
+            <div className="Review_div"><h4>{post.release_date}</h4></div>
+            <div className="Review_div"><h4>{post.director}</h4></div>
+            <div ><h4>{post.review}</h4></div>
+            <div className="Review_div"><h4>{post.rating}/5</h4></div>
+            <div className="Review_div"> <h4>{post.genre}</h4></div>
           </div>
           <button onClick={() => handleClick(post.id)}>⭐️</button>
           <Link to={`/postForm/${post.id}`}>
@@ -62,56 +62,6 @@ const Post = (props) => {
         </div>
       ))}
     </div>
-    // <div className="post_body">
-    //   <div className="wrapper">
-    //     <div className="table" >
-
-    //       <div className="row header">
-    //           <div className="cell">
-    //             Movie
-    //           </div>
-    //           <div className="cell">
-    //             Release Date
-    //           </div>
-    //           <div className="cell">
-    //             Director
-    //           </div>
-    //           <div className="cell">
-    //             Genre
-    //           </div>
-    //           <div className="cell">
-    //             Review
-    //           </div>
-    //           <div className="cell">
-    //             Rating
-    //           </div>
-    //       </div>
-
-    //       {posts.map((post) => (
-    //       <div className="row">
-    //           <div className="cell" data-title="Movie">
-    //           {post.title}
-    //           </div>
-    //           <div className="cell" data-title="Release Date">
-    //           {post.releaseDate}
-    //           </div>
-    //           <div className="cell" data-title="Director">
-    //           {post.director}
-    //           </div>
-    //           <div className="cell" data-title="Genre">
-    //           {post.genre}
-    //           </div>
-    //           <div className="cell" data-title="Review">
-    //           {post.review}
-    //           </div>
-    //           <div className="cell" data-title="Rating">
-    //             {post.rating}
-    //           </div>
-    //       </div>
-    //       ))}
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
 
--- client/src/services/index.jsx
+++ client/src/services/index.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+
 const apiURL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:8000"
